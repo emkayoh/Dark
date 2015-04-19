59f59a1e97a27bb0ba9f28434649e64cdb49dbc5
Jeremiah Kelly <emkayoh@mac.com>
2015-04-19 09:56:57 +0100

    corrected

--- R/AICc.R
+++ R/AICc.R
@@ -1,9 +1,1 @@
-AICc<-function(obj){
-  N<-length(obj$time)
-  P<-obj$Pn
-  if(is.null(obj$Pn))
-    P <- 7
-  SSE<-obj$val
-  AIC<-N*log(SSE/N)+2*P
-  AIC+(2*P*(P+1))/(N-P-1)
-}
+"close.connection" "close" "Rd2HTML" "<Anonymous>" "doTryCatch" "tryCatchOne" "tryCatchList" "tryCatch" "try" 
