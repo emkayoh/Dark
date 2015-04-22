4e830242b4d67e3ae7de5f6f962c061ca20f7f47
Jeremiah Kelly <emkayoh@mac.com>
2015-04-22 14:43:49 +0100

    edited

--- R/BestFit.R
+++ R/BestFit.R
@@ -1,39 +1,41 @@
 BestFit <- function(obj, MSC, draw) {
-	.GlobalEnv$x <- obj$time
-	.GlobalEnv$y <- obj$thrs
-	if (missing(draw)) 
-		draw = F
+  .GlobalEnv$x <- obj$time
+  .GlobalEnv$y <- obj$thrs
+  if (missing(draw))
+    draw <- F
 
-	mFn <- c(1, 1, P3, 1, P5c, 1, P7c)
-	idx <- c(3, 5, 7)
-	obj$call = NULL
+  mFn <- c(1, 1, P3, 1, P5c, 1, P7c)
+  idx <- c(3, 5, 7)
+  obj$call <- NULL
 
-	Mod <- which(MSC$AIC == min(MSC$AIC))
-	idx2 <- idx == Mod
-	Fn = mFn[[Mod]]
+  Mod <- which(MSC$AIC == min(MSC$AIC))
+  idx2 <- idx == Mod
+  Fn <- mFn[[Mod]]
 
-	oP <- MSC$param[idx2, ]
+  oP <- MSC$param[idx2, ]
 
-	Opt <- optim(oP, Fn)
-	while (Opt$con) Opt <- optim(Opt$par, Fn)
-	opt <- Opt$par
+  Opt <- optim(oP, Fn)
+  while (Opt$con) Opt <- optim(Opt$par, Fn)
+  opt <- Opt$par
 
-	if (draw) {
-		X <- seq(0, max(obj$time), length.out = 1000)
+  if (draw){
+    X <- seq(0, max(obj$time), length.out = 1000)
 
-		if (length(dev.list()) == 0) {
-			plot(obj$time, obj$thrs)
-		}
-		lines(X, Fn(opt, X), col = 2)
-	}
+    if (length(dev.list()) == 0) {
+      plot(obj$time, obj$thrs)
+    }
+    lines(X, Fn(opt, X), col = 2)
+  }
+  obj$val <- NULL
+  Res <- c(call = match.call(), obj,
+    list(opt = opt, Mod = Fn(Mod)$Mod,
+    Pn = Fn(Mod)$Pn, AIC = MSC$AIC, val = Opt$val))
+  Y <- Fn(opt, obj$time)
 
-	Res <- c(call = match.call(), obj, list(opt = opt, Mod = Fn(Mod)$Mod, Pn = Fn(Mod)$Pn, AIC = MSC$AIC, val = Opt$val))
-	Y <- Fn(opt, obj$time)
-
-	Res$fit <- Y
-	Res$resid <- obj$thrs - Y
-	Res$R2 <- 1 - (var(obj$thrs - Y)/var(obj$thrs))
-	on.exit(rm(list = c("x", "y"), envir = .GlobalEnv))
-	class(Res) = "dark"
-	Res
+  Res$fit <- Y
+  Res$resid <- obj$thrs - Y
+  Res$R2 <- 1 - (var(obj$thrs - Y) / var(obj$thrs))
+  on.exit(rm(list = c("x", "y"), envir = .GlobalEnv))
+  class(Res) <- "dark"
+  Res
 }
