6d9563f29169ec3a5c08666cc6d9ea807f23fae9
Jeremiah Kelly <emkayoh@mac.com>
2015-05-07 09:13:30 +0100

    improved appearance of the plot

--- R/tests/Workflow.R
+++ R/tests/Workflow.R
@@ -3,12 +3,12 @@
 rm(list=ls())
 load('data/Pugh_1975_Dark.Rdata')
 
-  par(mfrow=c(1,1))
+par(las=1, bty='n',mfrow=c(1,1))
 plot(tmp$time, tmp$thrs)
 
 P<-Start(tmp, 4000)
 MSC<-ModelSelect(tmp,P)
 tmp<-BestFit(tmp,MSC, T)
 tmp<-MultiStart(tmp,repeats = 25)
-tmp<-BootDark(tmp,R = 500)
+tmp<-BootDark(tmp,R = 500, T)
 
