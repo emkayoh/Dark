99d6b9b901fa29643f483cb497b8323bc6bf9dfa
Jeremiah Kelly <emkayoh@mac.com>
2015-08-19 09:50:23 +0100

    initial commit of P^ continuous model

---
+++ R/P6c.R
@@ -0,0 +1,19 @@
+P6c <- function(a, X) {
+  # Six Param biexponential model with a logistic transition
+ K<- -log10(exp(1)) # factor to convert from 1/Tau to S2
+  x <- .GlobalEnv$x
+  y <- .GlobalEnv$y # explicitly locating the values
+  
+  if (missing(X)) {
+    if (length(a) == 1) {
+      list(Pn = 6L, Mod = "P6c")
+    } else {
+      Yest <-  a[1]+a[2]*exp(-x*a[3]) +a[5]*exp(-(x-a[4])*H(x,20,a[4])*a[6])
+      sum((y - Yest) ^ 2)
+    }
+  } else {
+    x <- X
+    a[1]+a[2]*exp(-x*a[3]) +a[5]*exp(-(x-a[4])*H(x,20,a[4])*a[6])
+  }
+}
+
