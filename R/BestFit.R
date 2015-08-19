1dbf471239d54c76a65447e8c47703bf8848e4d3
Jeremiah Kelly <emkayoh@mac.com>
2015-08-19 09:47:11 +0100

    Added a six parameter bi exponential model

--- R/BestFit.R
+++ R/BestFit.R
@@ -4,8 +4,8 @@ BestFit <- function(obj, MSC, draw) {
   if (missing(draw)) 
     draw <- F
   
-  mFn <- c(1, 1, P3, 1, P5c, 1, P7c)
-  idx <- c(3, 5, 7)
+  mFn <- c(1, 1, P3, 1, P5c, P6c, P7c)
+  idx <- c(3, 5, 6, 7)
   obj$call <- NULL
   
   Mod <- which(MSC$AIC == min(MSC$AIC))
@@ -14,12 +14,12 @@ BestFit <- function(obj, MSC, draw) {
   
   oP <- MSC$param[idx2, ]
   
-  Opt <- optim(oP, Fn)
+  Opt <- optim(oP[1:Mod], Fn)
   while (Opt$con) Opt <- optim(Opt$par, Fn)
   opt <- Opt$par
   
   if (draw) {
-    X <- seq(0, max(obj$time), length.out = 1000)
+    X <- seq(0, max(obj$time), length.out = 100)
     
     if (length(dev.list()) == 0) {
       plot(obj$time, obj$thrs)
