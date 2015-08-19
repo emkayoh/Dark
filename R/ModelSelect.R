3790743fcc59de72466b5fa350b2e1f5b435b000
Jeremiah Kelly <emkayoh@mac.com>
2015-08-19 09:53:30 +0100

    Added fourth model and created a new START matix 'O'

--- R/ModelSelect.R
+++ R/ModelSelect.R
@@ -3,34 +3,66 @@ ModelSelect <- function(obj, P) {
 	.GlobalEnv$x <- obj$time
 	.GlobalEnv$y <- obj$thrs
 
-	param <- matrix(0, 3, 7)
-	idx <- c(3, 5, 7)
+	O <- P[,1:6]
+	O[,1] <- min(obj$thrs)
+	O[,2] <- P[,2]
+	O[,3] <- P[,3]
+	O[,4] <- P[,5]
+	O[,5] <- P[,2] 
+	O[,6] <- -P[,4]
+	
+	param <- matrix(0, 4, 7)
+	idx <- c(3, 5, 6, 7)
 	AIC <- numeric(7)
-	mFn <- c(1, 1, P3, 1, P5c, 1, P7c)
+	mFn <- c(1, 1, P3, 1, P5c, P6c, P7c)
 	jj = 1
 	
 	for (ii in idx) {
 		Fn = mFn[[ii]]
 
-		Out <- apply(P, 1, FUN = Fn)
-		oP <- P[which(Out == min(Out)), ]
-		oPval <- Out[which(Out == min(Out))]
-		Opt <- optim(oP[1:ii], Fn)
-		while (Opt$con) Opt <- optim(Opt$par, Fn)
+		if(ii ==6){
+		    Out <- apply(O, 1, Fn)
+		    oP <- O[which(Out == min(Out)), ]
+		    oPval <- Out[which(Out == min(Out))]
+		    Opt <- optim(oP[1:ii],Fn)
+		    while (Opt$con) Opt <- optim(Opt$par, Fn)
+		    
+		    init <- oP[1:Fn(1)$Pn]
+		    opt <- Opt$par[1:Fn(1)$Pn]
+		    param[jj, 1:Fn(1)$Pn] <- opt
+		    val <- Opt$val
+		    Pn <- Fn(1)$Pn
+		    Mod <- Fn(1)$Mod
+		    # builds the dark object to pass to AICc
+		    obj$init = init
+		    obj$opt = opt
+		    obj$val = val
+		    obj$Pn = Pn
+		    obj$Mod = Mod
+		    AIC[ii] <- AICc(obj)
+		  }else{
+		    Out <- apply(P, 1, FUN = Fn)
+		    oP <- P[which(Out == min(Out)), ]
+		    oPval <- Out[which(Out == min(Out))]
+		    Opt <- optim(oP[1:ii], Fn)
+		    while (Opt$con) Opt <- optim(Opt$par, Fn)
+		    
+		    init <- oP[1:Fn(1)$Pn]
+		    opt <- Opt$par[1:Fn(1)$Pn]
+		    param[jj, 1:Fn(1)$Pn] <- opt
+		    val <- Opt$val
+		    Pn <- Fn(1)$Pn
+		    Mod <- Fn(1)$Mod
+		    # builds the dark object to pass to AICc
+		    obj$init = init
+		    obj$opt = opt
+		    obj$val = val
+		    obj$Pn = Pn
+		    obj$Mod = Mod
+		    AIC[ii] <- AICc(obj)
+		  }
+		
 
-		init <- oP[1:Fn(1)$Pn]
-		opt <- Opt$par[1:Fn(1)$Pn]
-		param[jj, 1:Fn(1)$Pn] <- opt
-		val <- Opt$val
-		Pn <- Fn(1)$Pn
-		Mod <- Fn(1)$Mod
-		# builds the dark object to pass to AICc
-		obj$init = init
-		obj$opt = opt
-		obj$val = val
-		obj$Pn = Pn
-		obj$Mod = Mod
-		AIC[ii] <- AICc(obj)
 
 		jj = jj + 1
 	}
