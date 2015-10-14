0b5de8b1087b54bf41814488e37bff5f5c65b7f8
Jeremiah Kelly <emkayoh@mac.com>
2015-10-14 15:24:01 +0100

    model made easier to read

--- R/P6c.R
+++ R/P6c.R
@@ -8,12 +8,12 @@ P6c <- function(a, X) {
     if (length(a) == 1) {
       list(Pn = 6L, Mod = "P6c")
     } else {
-      Yest <-  a[1]+a[2]*exp(-x*a[3]) +a[5]*exp(-(x-a[4])*H(x,20,a[4])*a[6])
+      Yest <-  a[1]+a[2]*exp(-x*a[3]) +a[5]*exp(-(x-a[4])*a[6]*H(x,20,a[4]))
       sum((y - Yest) ^ 2)
     }
   } else {
     x <- X
-    a[1]+a[2]*exp(-x*a[3]) +a[5]*exp(-(x-a[4])*H(x,20,a[4])*a[6])
+    a[1]+a[2]*exp(-x*a[3]) +a[5]*exp(-(x-a[4])*a[6]*H(x,20,a[4]))
   }
 }
 
