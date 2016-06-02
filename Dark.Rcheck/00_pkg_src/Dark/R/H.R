270fdcd7da557a7f00b6f1892ac7d6bce9f31946
Jeremiah Kelly <emkayoh@mac.com>
2016-06-02 11:36:28 +0100

    deleted
    [ci skip]

--- Dark.Rcheck/00_pkg_src/Dark/R/H.R
+++
@@ -1,9 +0,0 @@
-#' @export 
-
-H <- function(x, k = 100, t) {
-# x is the measured time
-# k is the transition constant, set arbitrarily high
-# t is the time at which the transition occurs
-
-	round(1/(1 + exp(-2 * k * (x - t))), 1)
-}
